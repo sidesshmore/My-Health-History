42a23998fb4190bbfdc4c9250bce35b4dd182aac
sidesshmore <mosiddhesh@gmail.com>
2023-09-01 19:35:51 +0530

    CORS_ALLOW_ALL_ORIGINS = True

--- My_Health_History/settings.py
+++ My_Health_History/settings.py
@@ -53,9 +53,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-CORS_ALLOW_ALL_ORIGINS: True
 
-# CORS_ALLOWED_ORIGINS = ["*"]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
 
 CORS_ALLOW_METHODS = (
     "DELETE",
