f9ac9364f035fdbe5de684e7b83c3de32f04f67f
sidesshmore <mosiddhesh@gmail.com>
2023-09-01 18:47:26 +0530

    cors methods added

--- My_Health_History/settings.py
+++ My_Health_History/settings.py
@@ -57,7 +57,18 @@ CORS_ALLOW_ALL_ORIGINS: True
 
 CORS_ALLOWED_ORIGINS = ["*"]
 
+CORS_ALLOW_METHODS = (
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+)
+
 ROOT_URLCONF = 'My_Health_History.urls'
+
+
 
 TEMPLATES = [
     {
